abb986eb891dd8ce32bc9eb825db61525f2b6bd0
Xing Yun <ink.xing.yun@gmail.com>
2018-01-29 10:22:32 -0500

    dependency upgrade coding style

--- src/CirclePack/get-lines.js
+++ src/CirclePack/get-lines.js
@@ -1,22 +1,22 @@
 //From http://stackoverflow.com/questions/2936112/text-wrap-in-a-canvas-element
-let getLines =(ctx, text, maxWidth, fontSize, titleFont)=> {
-    let words = text.split(" ");
-    let lines = [];
-    let currentLine = words[0];
+let getLines = (ctx, text, maxWidth, fontSize, titleFont) => {
+  let words = text.split(' ');
+  let lines = [];
+  let currentLine = words[0];
 
-    for (let i = 1; i < words.length; i++) {
-        let word = words[i];
-        ctx.font = fontSize + "px " + titleFont;
-        let width = ctx.measureText(currentLine + " " + word).width;
-        if (width < maxWidth) {
-            currentLine += " " + word;
-        } else {
-            lines.push(currentLine);
-            currentLine = word;
-        }
+  for (let i = 1; i < words.length; i++) {
+    let word = words[i];
+    ctx.font = fontSize + 'px ' + titleFont;
+    let width = ctx.measureText(currentLine + ' ' + word).width;
+    if (width < maxWidth) {
+      currentLine += ' ' + word;
+    } else {
+      lines.push(currentLine);
+      currentLine = word;
     }
-    lines.push(currentLine);
-    return lines;
-}//function getLines
+  }
+  lines.push(currentLine);
+  return lines;
+}; //function getLines
 
 export default getLines;
--- src/util/elbow.js
+++ src/util/elbow.js
@@ -1,6 +1,5 @@
 const elbow = (d, i) => {
-    return "M" + d.parent.y + "," + d.parent.x
-        + "V" + d.x + "H" + (d.y - 0);
-}
+  return 'M' + d.parent.y + ',' + d.parent.x + 'V' + d.x + 'H' + (d.y - 0);
+};
 
 export default elbow;
