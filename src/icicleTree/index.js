a64f4545171edbb3d060e6f0ce0f2a6ad55faa46
Xing Yun <ink.xing.yun@gmail.com>
2018-03-25 21:56:14 -0400

    icicleTree is using composition

---
+++ src/icicleTree/index.js
@@ -0,0 +1,16 @@
+import { svgLayer, factory, DefaultCategoricalColor } from 'vizart-core';
+import apiRender from './api-render';
+import apiColor from './api-color';
+
+const opt = {
+  chart: {
+    type: 'icicle-tree',
+  },
+  color: DefaultCategoricalColor,
+};
+
+
+export default factory(svgLayer, {
+    opt,
+}, [apiRender, apiColor]);
+
